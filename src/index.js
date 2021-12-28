087f888a6e070d75e27d4a28244d8013d52172ce
Serhii Honchar <sehonc@softserveinc.com>
2021-12-28 22:24:02 +0200

    add adding

--- src/index.js
+++ src/index.js
@@ -1,9 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Header from './header.js'
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+class App extends React.Component {
+    constructor() {
+        super()
 
+        this.state = {
+            todos: [
+                'first',
+                'second',
+                'third'
+            ]
+        }
+    }
+
+    addTodo = (todo) => {
+        this.setState({
+            todos: [
+                ...this.state.todos,
+                todo
+            ]
+        })
+    }
+
+    render() {
+        return <div className="application">
+            <Header
+                addTodoMethod={this.addTodo}
+            />
+            <Body
+                todosArg={this.state.todos}
+            />
+        </div>
+    }
+}
+
+const Body = ({todosArg}) => {
+    return <div>
+        {todosArg.map((todo, i) => <div key={i}>{todo}</div>)}
+    </div>
+}
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -14,4 +53,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
